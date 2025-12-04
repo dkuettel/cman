de79b4d790b44f5bc618cf1338924c41bffaa145
Daniel Kuettel <dkuettel@gmail.com>
2025-12-04 14:50:00 +0100

    lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "cman";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:dkuettel/nixpkgs/stable";
     flake-utils.url = "github:numtide/flake-utils";
 
     # the python package https://boisgera.github.io/pandoc/ currently needs pandoc 3.2.1 or lower
